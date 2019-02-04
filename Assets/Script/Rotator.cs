3061ba9c577563426871417d5f61788d64ae7593
ChickyParrot <47043659+ChickyParrot@users.noreply.github.com>
2019-02-04 00:10:54 -0500

    Add files via upload

---
+++ Assets/Script/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 25f, 0) * Time.deltaTime);
+
+    }
+}
